a75305c273ce1a8b382a2b82370abee67ade35cb
angeal185 <beneaves01@hotmail.com>
2017-06-07 21:26:34 +0800

    commit

---
+++ app/js/tpl.js
@@ -0,0 +1,39 @@
+var data = [{
+    "url": "//www.mfiles.co.uk/mp3-downloads/korobeiniki.mp3",
+    "name": "Demo 01"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/Toccata-and-Fugue-Dm.mp3",
+    "name": "Demo 02"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/beethoven-minuet-in-G.mp3",
+    "name": "Demo 03"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/moonlight-movement1.mp3",
+    "name": "Demo 04"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/Dvorak-Symphony9-2-from-the-New-World.mp3",
+    "name": "Demo 05"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/land-of-hope-and-glory.mp3",
+    "name": "Demo 06"
+}, {
+    "url": "//www.mfiles.co.uk/mp3-downloads/mozart-piano-concerto-21-2-elvira-madigan-piano-solo.mp3",
+    "name": "Demo 07"
+}];
+
+var ctrl = [{
+    "class": "shuffle shuffleActive",
+    "attr": "shuffled"
+},{
+    "class": "left",
+    "attr": "prevAudio"
+},{
+    "class": "play",
+    "attr": "playPauseAudio"
+},{
+    "class": "right",
+    "attr": "nextAudio"
+},{
+    "class": "repeat",
+    "attr": "repeatSong"
+}];
