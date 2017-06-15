a526ce131ae138aad0941b84d0683ad2097123bb
Angeal185 <beneaves01@hotmail.com>
2017-06-15 13:06:00 +0800

    Create tpl.js

--- app/js/tpl.js
+++ app/js/tpl.js
@@ -1,23 +1,23 @@
 var data = [{
-    "url": "//www.mfiles.co.uk/mp3-downloads/korobeiniki.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/korobeiniki.mp3",
     "name": "Demo 01"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/Toccata-and-Fugue-Dm.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/Toccata-and-Fugue-Dm.mp3",
     "name": "Demo 02"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/beethoven-minuet-in-G.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/beethoven-minuet-in-G.mp3",
     "name": "Demo 03"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/moonlight-movement1.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/moonlight-movement1.mp3",
     "name": "Demo 04"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/Dvorak-Symphony9-2-from-the-New-World.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/Dvorak-Symphony9-2-from-the-New-World.mp3",
     "name": "Demo 05"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/land-of-hope-and-glory.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/land-of-hope-and-glory.mp3",
     "name": "Demo 06"
 }, {
-    "url": "//www.mfiles.co.uk/mp3-downloads/mozart-piano-concerto-21-2-elvira-madigan-piano-solo.mp3",
+    "url": "http://www.mfiles.co.uk/mp3-downloads/mozart-piano-concerto-21-2-elvira-madigan-piano-solo.mp3",
     "name": "Demo 07"
 }];
 
